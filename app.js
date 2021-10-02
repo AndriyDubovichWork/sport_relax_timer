aedc79929c87142aa21cc2d60802cbb72d661d01
Andrew <66020477+asadad2281@users.noreply.github.com>
2021-10-02 20:27:26 +0300

    add repeat number

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const audio = new Audio('alarm.mp3')
 
 let progressbar=document.getElementById('progressbar')
 let RelaxSport=document.getElementById('RelaxSport')
+let ReapeatNum=document.getElementById('numberOFreapeat')
 progressbar.style.width='90%'
 
 function Start(){
   let relaxtime=15
   let workTime=45
+  let repeatnum=7
+  
   if (document.getElementById('relaxTime').value) {
     relaxtime=document.getElementById('relaxTime').value
     
@@ -18,9 +21,14 @@ function Start(){
     workTime=document.getElementById('workTime').value
     
   }  
+  if (document.getElementById('numberOFreapeat').value) {
+    repeatnum=document.getElementById('numberOFreapeat').value
+    
+  }  
   document.getElementById('relaxTime').disabled=true
   document.getElementById('workTime').disabled=true
-
+  document.getElementById('numberOFreapeat').disabled=true
+  progressbar.max=repeatnum*20
 
 
 
@@ -35,7 +43,7 @@ function Start(){
   document. getElementById("start").disabled = true
   timer1=setTimeout(
     () => {
-      if(FuncchangeCount<14){
+      if(FuncchangeCount<repeatnum*2){
         
         
         document.body.style.background = "rgb(40,0,255)";
@@ -51,6 +59,7 @@ function Start(){
         document. getElementById("start").disabled = false
         document.getElementById('relaxTime').disabled=false
         document.getElementById('workTime').disabled=false
+        document.getElementById('numberOFreapeat').disabled=false
         progressbar.value=0
       }
     },
@@ -60,6 +69,7 @@ function Start(){
     document.getElementById("start").disabled = false
     document.getElementById('relaxTime').disabled=false
     document.getElementById('workTime').disabled=false
+    document.getElementById('numberOFreapeat').disabled=false
     progressbar.value=0
     clearTimeout(timer2);
     clearTimeout(timer1);
@@ -73,7 +83,7 @@ function Start(){
   document. getElementById("start").disabled = true
   timer2=setTimeout(
     () => {
-      if(FuncchangeCount<14){
+      if(FuncchangeCount<repeatnum*2){
         
         document.body.style.background = "rgb(50,255,0)";
         document.body.style.background = "linear-gradient(90deg, rgba(50,255,0,1) 0%, rgba(0,212,255,1) 100%)";
@@ -87,6 +97,7 @@ function Start(){
         document. getElementById("start").disabled = false
         document.getElementById('relaxTime').disabled=false
         document.getElementById('workTime').disabled=false
+        document.getElementById('numberOFreapeat').disabled=false
         progressbar.value=0
       }
     },
@@ -96,6 +107,7 @@ function Start(){
     document.getElementById("start").disabled = false
     document.getElementById('relaxTime').disabled=false
     document.getElementById('workTime').disabled=false
+    document.getElementById('numberOFreapeat').disabled=false
     progressbar.value=0
     clearTimeout(timer2);
     clearTimeout(timer1);
